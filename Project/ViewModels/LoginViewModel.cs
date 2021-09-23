7e26a86efb8d1bc3da06ac52b132b9d22c6e24f3
Misha902 <79799958+Misha902@users.noreply.github.com>
2021-09-23 19:39:25 +0300

    Some views fix

--- Project/ViewModels/LoginViewModel.cs
+++ Project/ViewModels/LoginViewModel.cs
@@ -16,5 +16,6 @@ namespace CosmoShop.ViewModels
         [Display(Name = "Password")]
         public string Password { get; set; }
         public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
     }
 }
